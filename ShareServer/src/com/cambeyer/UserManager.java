c2ab9660c8c14b4adcb0b51dad9f75a027581492
cbeyer-riis <cbeyer@riis.com>
2014-09-22 20:38:46 -0400

    changed miles to meters in the server's distance conversion function

--- ShareServer/src/com/cambeyer/UserManager.java
+++ ShareServer/src/com/cambeyer/UserManager.java
@@ -57,7 +57,8 @@ public class UserManager {
 	}
 	
 	private static double distFrom(double lat1, double lng1, double lat2, double lng2) {
-	    double earthRadius = 3958.75;
+		double earthRadius = 6371; //kilometers
+	    //double earthRadius = 3958.75; //miles
 	    double dLat = Math.toRadians(lat2-lat1);
 	    double dLng = Math.toRadians(lng2-lng1);
 	    double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLng/2) * Math.sin(dLng/2);
